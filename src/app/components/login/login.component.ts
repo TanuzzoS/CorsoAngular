f2c09207ae91afad2f8107258fc06065f8f082d9
Syro <73841248+TanuzzoS@users.noreply.github.com>
2026-02-27 11:39:54 +0100

    Add files via upload

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule, RouterLink],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+  submitted = false;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  get email() { return this.loginForm.get('email'); }
+  get password() { return this.loginForm.get('password'); }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.loginForm.valid) {
+      console.log('Login:', this.loginForm.value);
+      alert('Login effettuato con successo!');
+    }
+  }
+}
